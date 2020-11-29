9b86e55cdd8c3faab49ead7e979e4c3d712e2486
dewarifal <75195987+dewarifal@users.noreply.github.com>
2020-11-29 13:33:17 +0700

    Add files via upload

---
+++ Assets/UTS/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed;
+	private float waktuTunggu;
+	public float startWaktuTunggu;
+
+	public Transform moveSpot;
+	public float minX;
+	public float maxX;
+	public float minY;
+	public float maxY;
+	public float minZ;
+	public float maxZ;
+
+	// Use this for initialization
+	void Start () {
+		waktuTunggu = startWaktuTunggu;
+
+		moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, speed = Time.deltaTime);
+
+		if(Vector3.Distance(transform.position, moveSpot.position) < 0.2f){
+			if(waktuTunggu <= 0){
+				moveSpot.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ));
+				waktuTunggu = startWaktuTunggu;
+			} else
+			{
+				waktuTunggu -= Time.deltaTime;
+			}
+		}
+	}
+}
---
+++ Assets/UTS/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
+
+    CharacterController chara;
+
+	void Start () {
+        chara = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        float motionX = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float motionZ = Input.GetAxis("Vertical") * Time.deltaTime; ;
+
+        Vector3 motion = new Vector3(motionX, 0f, motionZ);
+
+        chara.Move(motion);
+    }
+
+    
+}
